fd7d9fb811137c44c9c9f3891562e5f94bf3cd67
ErikBezerraC <erikbezerra80@gmail.com>
2026-08-07 17:23:08 -0300

    diminui config

--- lib/Telas/config/config.dart
+++ lib/Telas/config/config.dart
@@ -42,7 +42,7 @@ class _ConfigState extends State<Config> {
           padding: const EdgeInsets.only(top: 10),
           child: Container(
             width: 360,
-            height: 475,
+            height: 410,
             decoration: BoxDecoration(
               color: const Color(0xff7a9b6e),
               borderRadius: BorderRadius.circular(15),
