7f97c57f40c3dd99d8ef83d080341e4803e421a1
ErikBezerraC <erikbezerra80@gmail.com>
2026-08-07 16:23:31 -0300

    atualizaçõa da tela de configurações

---
+++ lib/Telas/config/usuario.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Usuario extends StatefulWidget {
+  const Usuario({super.key});
+
+  @override
+  State<Usuario> createState() => _UsuarioState();
+}
+
+class _UsuarioState extends State<Usuario> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+        backgroundColor: Color(0xff87a277),
+      ),
+
+      backgroundColor: Color(0xff87a277),
+
+    );
+  }
+}
--- lib/Telas/login.dart
+++ lib/Telas/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Login extends StatefulWidget {
   const Login({super.key});
 
